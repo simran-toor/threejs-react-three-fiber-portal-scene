a5e63800768d649910c6f14cf8c6184e1eadd848
Simran Toor <simrankaurtoor@outlook.com>
2024-09-12 17:26:11 +0100

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,71 @@
+import { shaderMaterial, Sparkles, Center, useTexture, useGLTF, OrbitControls } from '@react-three/drei'
+import portalVertexShader from './shaders/portal/vertex.glsl'
+import portalFragmentShader from './shaders/portal/fragment.glsl'
+import * as THREE from 'three'
+import { useFrame, extend } from '@react-three/fiber'
+import { useRef } from 'react'
+
+const PortalMaterial = shaderMaterial(
+    {
+        uTime: 0,
+        uColorStart: new THREE.Color('#ffffff'),
+        uColorEnd: new THREE.Color('#000000')
+    },
+    portalVertexShader,
+    portalFragmentShader
+)
+
+extend({ PortalMaterial })
+
+export default function Experience()
+{
+    const { nodes } = useGLTF('./model/portal.glb')
+
+    const bakedTexture = useTexture('./model/baked.jpg')
+    bakedTexture.flipY = false
+
+    const portalMaterial = useRef()
+    useFrame((state, delta) =>
+    {
+        portalMaterial.current.uTime += delta
+    })
+
+    return <>
+
+        <color args={ ['#030202'] } attach="background" />
+
+        <OrbitControls makeDefault />
+
+        <Center>
+            <mesh geometry={ nodes.baked.geometry } >
+                <meshBasicMaterial map={ bakedTexture } />
+            </mesh>
+
+            <mesh geometry={ nodes.poleLightA.geometry } position={ nodes.poleLightA.position } >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            <mesh geometry={ nodes.poleLightB.geometry } position={ nodes.poleLightB.position } >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
+
+            <mesh 
+                geometry={ nodes.portalLight.geometry } 
+                position={ nodes.portalLight.position }
+                rotation={ nodes.portalLight.rotation }
+            >
+                <portalMaterial ref={ portalMaterial } />
+            </mesh>
+
+            <Sparkles
+                size={ 6 }
+                scale={ [4, 2, 4] }
+                position-y={ 1 }
+                speed={ 0.2 }
+                count={ 40 }
+            />
+
+        </Center>
+
+    </>
+}
